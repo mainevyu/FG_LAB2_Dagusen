58ae80a3c02ae1447618c65da3950d36608758bc
Charmaine D. Dagusen <20255342@s.ubaguio.edu>
2026-02-02 14:48:50 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: '/mainevyu.github.io/FG_LAB2_Dagusen/',
+  plugins: [react()]
 })
